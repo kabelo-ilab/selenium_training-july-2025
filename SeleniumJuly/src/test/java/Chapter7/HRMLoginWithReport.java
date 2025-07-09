7375bab103ff4be33a64e8ecf9aaa07f6e8a6108
kabelo-ilab <Kabelo.tlhape@ilabqa.com>
2025-07-09 17:08:11 +0200

    Chapter 7 completed with reports, screenshots, tags and method execution order

--- SeleniumJuly/src/test/java/Chapter7/HRMLoginWithReport.java
+++ SeleniumJuly/src/test/java/Chapter7/HRMLoginWithReport.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -26,11 +27,12 @@ public class HRMLoginWithReport {
    static ExtentReports report;
    static ExtentSparkReporter rptSpark;
    boolean isPass;
+   static String reportURL = "src/test/resources/Reports/Spark.html";
 
    @BeforeAll
    static void setup(){
        report = new ExtentReports();
-       rptSpark = new ExtentSparkReporter("src/test/resources/Reports/Spark.html");
+       rptSpark = new ExtentSparkReporter(reportURL);
        rptSpark.config().setReportName("Test Summary Report");
        rptSpark.config().setDocumentTitle("The Spark Report");
        report.attachReporter(rptSpark);
@@ -41,28 +43,41 @@ public class HRMLoginWithReport {
 
    @AfterEach
    void generateReport(TestInfo info){
-       ExtentTest myTest = report.createTest(info.getTestMethod().get().getName());
-       if (isPass){
-           myTest.log(Status.PASS, "Test passed");
-       }else{
-           myTest.log(Status.FAIL,"Test failed");
+       ExtentTest myTest = report.createTest(info.getDisplayName());
+
+       try{
+           Thread.sleep(3000);
+           String image = ((TakesScreenshot)(driver)).getScreenshotAs(OutputType.BASE64);
+
+           if (isPass){
+               myTest.log(Status.PASS, "Test passed");
+           }else{
+               myTest.log(Status.FAIL,"Test failed ");
+           }
+
+           myTest.addScreenCaptureFromBase64String(image);
+
+           driver.manage().deleteAllCookies();
+       }catch (InterruptedException e){
+           System.out.println(e.getMessage());
        }
+
    }
 
-    @ParameterizedTest(name = "TC1 - Valid Login - [{index}] : {arguments}")
-    @CsvSource(value = {"Admin,admin123"})
-    @Order(1)
-    void testValidLogin(String username, String password){
+   @Test
+   @DisplayName("TC1 - Valid Login")
+   @Order(1)
+   @Tag("UAT")
+    void testValidLogin(){
         //Arrange
         String expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
 
-        //WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 
-        driver.findElement(By.name("username")).sendKeys(username);
-        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         //Actual
         String actualURL = driver.getCurrentUrl();
@@ -71,19 +86,42 @@ public class HRMLoginWithReport {
         assertEquals(expectedURL, actualURL);
     }
 
+//    @Disabled
+//    @ParameterizedTest(name = "TC1 - Valid Login - [{index}] : {arguments}")
+//    @CsvSource(value = {"Admin,admin123"})
+//    @Order(1)
+//    void testValidLogin(String username, String password){
+//        //Arrange
+//        String expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+//
+//        //WebDriver driver = new ChromeDriver();
+//        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+//
+//        driver.findElement(By.name("username")).sendKeys(username);
+//        driver.findElement(By.name("password")).sendKeys(password);
+//        driver.findElement(By.cssSelector("button[type='submit']")).click();
+//        //Actual
+//        String actualURL = driver.getCurrentUrl();
+//        isPass = driver.getCurrentUrl().equals(expectedURL);
+//        //Assert
+//        assertEquals(expectedURL, actualURL);
+//    }
+
     @ParameterizedTest(name = "TC2 - Invalid Login - [{index}] : {arguments}")
     @CsvSource(value = {"Admin,admin12345", "Admin12,admin123", "TestAdmin,TestPassword"})
     @Order(2)
+    @Tag("UAT")
+    @Tag("System")
     void testInvalidLogin(String username, String password){
         //Arrange
         String expectedText = "Invalid credentials";
 
-        //WebDriver driver = new ChromeDriver();
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        //driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
         driver.findElement(By.name("username")).sendKeys(username);
         driver.findElement(By.name("password")).sendKeys(password);
@@ -102,16 +140,15 @@ public class HRMLoginWithReport {
     @ParameterizedTest(name = "TC3 - Invalid Login - [{index}] : {arguments}")
     @CsvSource(value = {"Admin,admin12345", "Admin12,admin123", "TestAdmin,TestPassword"})
     @Order(3)
+    @Tag("System")
     void testInvalidLoginNegative(String username, String password){
         //Arrange
         String expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
 
-       // WebDriver driver = new ChromeDriver();
         driver.switchTo().newWindow(WindowType.TAB);
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        //driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
         driver.findElement(By.name("username")).sendKeys(username);
         driver.findElement(By.name("password")).sendKeys(password);
@@ -126,7 +163,18 @@ public class HRMLoginWithReport {
     @AfterAll
     static void tearDown(){
        report.flush();
-    }
 
+       try{
+           Thread.sleep(2000);
+           driver.quit();
+           Desktop.getDesktop().browse(new File(reportURL).toURI());
+
+       }catch (IOException ex){
+           System.out.println(ex.getMessage());
+       } catch (InterruptedException e) {
+           System.out.println(e.getMessage());
+       }
+
+    }
 
 }
